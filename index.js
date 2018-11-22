74b31f03c2bdbfd5c02b83c0604c43c3301c1745
danielpreys <danielpreys@yahoo.com>
2018-11-22 14:19:02 -0800

    add application

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname + "/public")))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.sendFile(__dirname +  '/views/index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+ 
